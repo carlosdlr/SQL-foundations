e4f8a57a51a8777dfd5ec07278b077d4f493f78d
carlos andres de la rosa <kusanagi12002@gmail.com>
2020-05-11 19:02:51 +0200

    data control language topic

---
+++ data_control_language.sql
@@ -0,0 +1,13 @@
+-- Data Control Language (DCL)
+-- User needs permission to perform database operations
+-- Privileges: Add user, Create databases, create tables, perform quieries
+-- the GRANT statement & REVOKE statement are used to perform users permissions administration
+-- GRANT: give the permission
+-- REVOKE: withdraw privileges
+
+
+-- GRANT statement
+-- gives permission/privileges
+-- set privileges to limited databases or all databases
+-- give selected number of privileges or all privileges
+-- GRANT privilegename ON databasename TO username;   
